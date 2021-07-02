b7601de2be7b4f4f85dd4232aab61510b3c764aa
Héctor Francés <32682445+hfrances@users.noreply.github.com>
2021-07-02 12:48:41 +0200

    Remove HttpClientHelper + Add Test project + Update azure-pipelines template.

---
+++ qckdev.AspNetCore.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace qckdev.AspNetCore.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.Inconclusive();
+        }
+    }
+}
